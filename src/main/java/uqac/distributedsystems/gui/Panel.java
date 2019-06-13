2f1a41d591482e9e4eb7f49e813e40a59c2cc1ad
LouisBertin <bertin.louis7@gmail.com>
2019-06-13 18:49:43 -0400

    display room on gui works

--- src/main/java/uqac/distributedsystems/gui/Panel.java
+++ src/main/java/uqac/distributedsystems/gui/Panel.java
@@ -1,30 +1,38 @@
 package uqac.distributedsystems.gui;
 
+import uqac.distributedsystems.model.Coordinate;
+import uqac.distributedsystems.model.Room;
+
 import java.awt.Color;
 import java.awt.Dimension;
-import java.awt.FlowLayout;
 import java.awt.Graphics;
 
-import javax.swing.JLabel;
 import javax.swing.JPanel;
 
+/**
+ * The type Panel.
+ */
 public class Panel extends JPanel {
 
+	/**
+	 * Instantiates a new Panel.
+	 *
+	 * @param d the d
+	 */
 	Panel(Dimension d){
 		System.out.println(d);
 		this.setSize(d);
 	}
 
 	public void paintComponent(Graphics g){
-		g.drawString("Kitchen", 10, 12);
 		//x1, y1, width, height
-		g.setColor(Color.BLUE);
-		g.fillRect(10, 15, 100, 100);
+		Room room = new Room("kitchen", new Coordinate(0, 0), 200, 200, Color.BLUE);
+		room.paintComponent(g);
 
 		//object dot :
-		g.setColor(Color.DARK_GRAY);
+/*		g.setColor(Color.RED);
 		g.fillOval(20, 20, 8, 8);
-		g.drawString("Wi-Fi Direct", 35, 35);
+		g.drawString("Wi-Fi Direct", 35, 35);*/
 	}
 
 }
